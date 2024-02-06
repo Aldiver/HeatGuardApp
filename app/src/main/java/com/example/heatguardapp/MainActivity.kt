6e02a55338c3a1f53d442e2e4ed4455794d35cde
Aldiver <aldiver.alcoriza@gmail.com>
2024-02-07 00:18:20 +0800

    some random things

--- app/src/main/java/com/example/heatguardapp/MainActivity.kt
+++ app/src/main/java/com/example/heatguardapp/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.heatguardapp
 
-import android.bluetooth.le.BluetoothLeScanner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -32,10 +36,13 @@ fun WelcomeScreen(){
 
 @Composable
 fun MessageCard(name: String) {
+    val expanded = remember { (mutableStateOf<Boolean>(false)) }
     Surface(color = MaterialTheme.colorScheme.primary) {
         Column {
             Text(text = "Hello, ", modifier = Modifier.padding(24.dp))
-            Text(text = name, modifier = Modifier.padding(24.dp))
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if(expanded.value) "True Value" else "False Value", color = Color.Black)
+            }
         }
     }
 }
