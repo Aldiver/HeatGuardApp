013b2262e8d54988de04fa16fe48f025a7dc1484
aldiver <aldiver.alcoriza@gmail.com>
2024-02-07 17:08:39 +0800

    new stuff

--- app/src/main/java/com/example/heatguardapp/MainActivity.kt
+++ app/src/main/java/com/example/heatguardapp/MainActivity.kt
@@ -3,18 +3,19 @@ package com.example.heatguardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.material.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,33 +23,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           WelcomeScreen()
+           OnboardingScreen()
+        }
+    }
+}
+@Composable
+fun BasicsCodelabTheme(content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun WelcomeScreen(){
-    Surface(color = MaterialTheme.colorScheme.primary){
-        MessageCard(name = "Al Nabigh")
-    }
-}
-
-@Composable
-fun MessageCard(name: String) {
-    val expanded = remember { (mutableStateOf<Boolean>(false)) }
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column {
-            Text(text = "Hello, ", modifier = Modifier.padding(24.dp))
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if(expanded.value) "True Value" else "False Value", color = Color.Black)
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMessageCard() {
-    WelcomeScreen()
+fun OnboardingPreview() {
+    OnboardingScreen()
 }
